505b55705509fb765ec92d91aaf36d6c9884d1ff
hhnchau <kingpes@kingpess-mbp.mshome.net>
2018-08-05 22:47:49 +0700

    commit

--- AlertIos/ViewController.swift
+++ AlertIos/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+
+    @IBAction func btnShowAlert(_ sender: Any) {
+        var alertController: UIAlertController?
+        alertController = UIAlertController(title: "Alert", message: "Are you sure?", preferredStyle: .alert) //Center
+        //alertController = UIAlertController(title: "Alert", message: "Are you sure?", preferredStyle: .actionSheet) //Bottom
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive){ (action) in
+            print("Press Cancel")
+        }
+        let ok = UIAlertAction(title: "OK", style: .default){ (action) in
+            print("Press OK")
+        }
+        
+        alertController?.addAction(cancel)
+        alertController?.addAction(ok)
+        
+        self.present(alertController!, animated: true){
+            print("Alert Show")
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
