3f854d5eaeb0be16037ed9cc978ef4d87c3987d6
hhnchau <kingpes@kingpess-macbook-pro.local>
2018-08-05 22:25:05 +0700

    Initial Commit

---
+++ AlertIos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlertIos
+//
+//  Created by Kingpes on 8/5/18.
+//  Copyright © 2018 Kingpes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
